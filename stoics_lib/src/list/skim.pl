a90a55ffd160405c1592201713746c93afc821b3
Nicos Angelopoulos <na11@sanger.ac.uk>
2018-02-14 17:00:22 +0000

    DOC:      examples in skim/3

--- stoics_lib/src/list/skim.pl
+++ stoics_lib/src/list/skim.pl
@@ -1,8 +1,27 @@
-
 /** skim( +Nested, -Scum, -Remains ).
 
-	Skim the first elements (Scum) from a Nested list with the tails 
-	being the Remains.
+	Skim the first elements (Scum) from a Nested list with the tails being the Remains.<br>
+    Fails if Nested has no more elements to skim at all positions (typically a list of empty lists).
+
+==
+?- Nest = [[a,b,c],[1,2,3]], skim( Nest, Sc, Rest ).
+Nest = [[a, b, c], [1, 2, 3]],
+Sc = [a, 1],
+Rest = [[b, c], [2, 3]].
+
+?- Nest = [[a,b,c],[1,2,3]], skim(Nest,Sc1,Rest1), skim(Rest1,Sc2,Rest2), skim(Rest2,Sc3,Rest3).
+Nest = [[a, b, c], [1, 2, 3]],
+Sc1 = [a, 1],
+Rest1 = [[b, c], [2, 3]],
+Sc2 = [b, 2],
+Rest2 = [[c], [3]],
+Sc3 = [c, 3],
+Rest3 = [[], []].
+
+?- Nest = [[a,b,c],[1,2,3]], skim(Nest,Sc1,Rest1), skim(Rest1,Sc2,Rest2), skim(Rest2,Sc3,Rest3), skim(Rest3,Sc4,Rest4).
+false.
+
+==
 */
 skim([], [], [] ).
 skim([[H|T]|M], [H|RScum], [T|R] ) :-
