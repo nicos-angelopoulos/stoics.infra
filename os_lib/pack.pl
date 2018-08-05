41654ef24b406e85f78e938af404944b2a5105b0
Nicos Angelopoulos <nicos.angelopoulos@gmail.com>
2018-08-05 13:24:33 +0100

    Version  1.1 (1:1:0)

--- os_lib/pack.pl
+++ os_lib/pack.pl
@@ -1,6 +1,6 @@
 name( os_lib ).
 title( 'Operating system interaction predicates.' ).
-version( '1.0' ).
+version( '1.1' ).
 author( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
 packager( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
 maintainer( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
