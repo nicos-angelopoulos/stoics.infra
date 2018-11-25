acc8a90d59e69f1f1d8810ffc4fc80e16da4ed03
Nicos Angelopoulos <nicos@gmx.co.uk>
2018-11-25 15:28:18 +0000

    IFACE:  use portray_clauses/3 with defaults [quoted(false)]

--- stoics_lib/src/term/portray_clauses.pl
+++ stoics_lib/src/term/portray_clauses.pl
@@ -5,37 +5,56 @@ OptS can be a list or single option term from the following:
 
 Opts
   * mode(Mode=append)
-     append or write
+    append or write
 
   * stream(Stream)
-     default is user_output
+    default is user_output
 
   * file(File)
-     if present, overwrites Stream. if pack(by_unix) is present File will be
-     passed through by_unix_term_to_serial/2 before passed to open/3
+    if present, overwrites Stream.
+
+  * write_opts(WOpts=[])
+    options to pass to portray_clause/3. when [] is given, portray_clause/2 is used.
+
+==
+?- portray_clauses( [a(b,c),b(d,e),c(f,g.t)], [] ).
+a(b, c).
+b(d, e).
+c(f, g.t).
+true.
+==
 
 @author nicos angelopoulos
 @version  0.1 2016/12/10
+@version  0.2 2018/12/10, removed some private code reference and added WOpts
 
 */
 
 :- lib(en_list/2).
-:- lib(current_call/2).
-:- lib(suggests(by_unix)).
 
 portray_clauses( List, Opt ) :-
-     en_list( Opt, Opts ),
-     ( memberchk(file(FilePrv),Opts) ->
-          ( memberchk(mode(Mode),Opts) -> true; Mode = write ),
-		current_call( by_unix_term_to_serial(FilePrv,File), File=FilePrv ),
-          open( File, Mode, Out )
-          ;
-          ( memberchk(stream(Out),Opts) -> true; Out= user_output )
-     ),
-     portray_clauses_onto( List, Out ),
-     ( var(File) -> true; close( Out ) ).
+    en_list( Opt, Opts ),
+    ( memberchk(file(File),Opts) ->
+        ( memberchk(mode(Mode),Opts) -> true; Mode = write ),
+        open( File, Mode, Out )
+        ;
+        ( memberchk(stream(Out),Opts) -> true; Out= user_output )
+    ),
+    ( memberchk(write_opts(WOpts), Opts) -> true; WOpts = [quoted(true)] ),
+    portray_clauses_opts( WOpts, List, Out ),
+    ( var(File) -> true; close( Out ) ).
 
-portray_clauses_onto( [], _ ).
+portray_clauses_opts( [], List, Out ) :- !,
+    portray_clauses_onto( List, Out ).
+portray_clauses_opts( WOpts, List, Out ) :-
+    portray_clauses_onto_opts( List, Out, WOpts ).
+
+portray_clauses_onto( [], _Out ).
 portray_clauses_onto( [H|T], Out ) :-
-	portray_clause( Out, H ),
-	portray_clauses_onto( T, Out ).
+    portray_clause( Out, H ),
+    portray_clauses_onto_opts( T, Out ).
+
+portray_clauses_onto_opts( [], _, _WOpts ).
+portray_clauses_onto_opts( [H|T], Out, WOpts ) :-
+    portray_clause( Out, H, WOpts ),
+    portray_clauses_onto_opts( T, Out, WOpts ).
