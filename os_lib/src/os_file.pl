7bf43801db94480c060836665401e5415435fd1e
nicos angelopoulos <nicos@gmx.co.uk>
2019-09-26 21:41:54 +0100

    RETAB

--- os_lib/src/os_file.pl
+++ os_lib/src/os_file.pl
@@ -72,9 +72,9 @@ os_file( File ) :-
 
 os_file( File, _Opts ) :-
     % fixme: make it play with Opts
-	ground( File ),
-	!,
-	os_exists( File, type(flink) ).
+   ground( File ),
+   !,
+   os_exists( File, type(flink) ).
 os_file( File, Args ) :-
     options_append( os_file, Args, Opts ),
     options( [dir(Dir),dots(Dots),solutions(Sol),stem(Stem),sub(Sub)], Opts ),
@@ -88,9 +88,9 @@ os_file_sol( findall, Files, Dir, Abs, Stem, Dots, Sub ) :-
 
 os_file( File, Rel, Dir, Abs, Stem, Dots, Sub ) :-
     os_cast( Dir, +SysDir ),
-	directory_files( SysDir, EntriesUno ),
+   directory_files( SysDir, EntriesUno ),
     sort( EntriesUno, Entries ),
-	member( Entry, Entries ),
+   member( Entry, Entries ),
     Entry \== '.', Entry \== '..',
     os_file_dot( Dots, Entry ),
     os_path( Dir, Entry, Desc ),
@@ -102,10 +102,10 @@ os_file_dot( false, Os ) :-
     \+ atom_concat( '.', _, Os ).
 
 os_file_obj( Desc, Rel, Entry, File, _Dir, Abs, Stem, _Dots, _Sub ) :-
-	os_exists( Desc, [type(flink),err(test)] ),
+   os_exists( Desc, [type(flink),err(test)] ),
     !,
     ( Stem == false ->
-	    os_cast( Entry, File )
+       os_cast( Entry, File )
         ;
         ( Stem == abs ->
             os_path( Abs, Entry, Path ),
@@ -116,7 +116,7 @@ os_file_obj( Desc, Rel, Entry, File, _Dir, Abs, Stem, _Dots, _Sub ) :-
         )
     ).
 os_file_obj( Desc, Rel, Entry, File, _Dir, Abs, Stem, Dots, true ) :-
-	os_exists( Desc, type(dlink) ),
+   os_exists( Desc, type(dlink) ),
     os_path( Abs, Entry, Rbs ),
     os_file( File, Rel, Desc, Rbs, Stem, Dots, true ).
 
@@ -141,8 +141,8 @@ os_files_defaults( [dir('.'),sub(false)] ).
 % @see os_file/2
 %
 os_files( Files ) :-
-	findall( File, os_file(File), Files ).
+   findall( File, os_file(File), Files ).
 os_files( Files, Opts ) :-
-    % just so we get the version() option updated:
-    options_append( os_file, Opts, _All ),
-	findall( File, os_file(File,Opts), Files ).
+   % just so we get the version() option updated:
+   options_append( os_file, Opts, _All ),
+   findall( File, os_file(File,Opts), Files ).
