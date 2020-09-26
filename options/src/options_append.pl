b768a09bf10cf75330ba9f22d5bd0c412ff67df5
Nicos Angelopoulos <nicos@gmx.co.uk>
2020-09-27 00:40:23 +0100

    OPTS:    module based debug switching on

--- options/src/options_append.pl
+++ options/src/options_append.pl
@@ -9,7 +9,7 @@ options_append_known_process_option( version ).
 
 Look for PredName_defaults/1 (Defs) and if that exists append its argument to the end of OptS to get All.<br>
 OptS is casted to a list before the append, so single terms are allowed as options.<br>
-In addition, if file user_profile(stoics/options/Pname[.pl]) exists, its terms are appended between OptS and
+In addition, if file user_profile(stoics/options/PredName[.pl]) exists, its terms are appended between OptS and
 the argument of PredName_defaults/1.<br>
 Appending of the profile terms, can be by passed with options append_profile(false).<br>
 Listens to debug(options_append).
@@ -50,10 +50,15 @@ OAopts term or list of
      instantiates to all input options that do not have matching default
      term structure
 
+  * module(DbgMod=false)
+    any value apart from _false_ signals DbgMod(PredName) is used in the module.
+
   * debug(Dbg=none)
-     if _true_ debug this call, and call prolog_debug:debug_topic(Pname).
-     if _false_ trun debugging off for this call.
-     Else Dbg can be a debug term (other than none,false,true) or list of debug terms.
+    if _true_ then if DbgMod is _false_ then debug PredName and call prolog_debug:debug_topic(PredName).
+    Else, debug(DbgMod(PredName) is enabled.
+    if _false_ turn debugging off for this call.
+    if _all_, then if DbgMod is _false_ debug(_) is enabled else debug(DbgMod(_)) is enabled.
+    Else Dbg can be a debug term (other than none,false,true) or list of debug terms.
  
   * funnel(Proccess)
      as process() below,
@@ -84,7 +89,7 @@ OAopts term or list of
         Prior is the prior status of debug(PredName). For each element of Dbgs call RHS:
 
     * true
-        debug(PredName) same as
+        same as debug(PredName)
 
     * false
         nodebug(PredName) same as
@@ -132,7 +137,8 @@ The default OAopts list is [funnel(debug)].
   
 @author nicos angelopoulos
 @version  0.2 2014/9/20
-@version  0.3 2020/5/9, changed file location to: user_profile(stoics/options/Pname[.pl])
+@version  0.3 2020/5/9, changed file location to: user_profile(stoics/options/PredName[.pl])
+@version  0.4 2020/9/27, termed debug option
 @tbd  add option! for making sure that only recognised options (with additional,"silent" defaults) are accepted ??
 @see ~/bin/cline/keep.pl for debug option example
 @see pack(options/examples/ex_app.pl)
@@ -161,14 +167,15 @@ options_append( Pname, ArgS, Opts, OAoptS ) :-
     ( select(remove_types(RmvTypes),OAoptsNT,OAoptsNR) -> true; RmvTypes=true, OAoptsNR=OAoptsNT ),
     ( select(arity(PArity),OAoptsNR,OAoptsAri) -> true; PArity= -1, OAoptsAri=OAoptsNR ),
     ( select(pack(Pack),OAoptsAri,OAoptsPack) -> true; Pack=[], OAoptsPack=OAoptsAri ),
-    ( select(append_profile(AppProf),OAoptsPack,OAoptsApF) -> true; AppProf=true, OAoptsApF=OAoptsPack ),
+    ( select(module(DbgMod),OAoptsPack,OAoptsDmod) -> true ;  DbgMod=false, OAoptsPack=OAoptsDmod ),
+    ( select(append_profile(AppProf),OAoptsPack,OAoptsApF) -> true; AppProf=true, OAoptsApF=OAoptsDmod),
     OAopts = OAoptsApF,
     options_append_profile_options( AppProf, Pname, Args, ProfArgs ),
     findall( Xarg, member(extra_arg(Xarg),OAopts), Xargs ),
     append( ProfArgs, Xargs, ExtArgs ),
     options_def_append( Dname, Pname, ProfArgs, ExtArgs, Defs, OptsUnpro ),
     options_append_select_own_debug( OAopts, ProcessOpts, Restore ),
-    options_append_process( ProcessOpts, OptsUnpro, Args, Defs, Pname, OptsWT ),
+    options_append_process( ProcessOpts, OptsUnpro, Args, Defs, Pname, DbgMod, OptsWT ),
     options_append_types( ChkTypes, Pname/PArity, Pack, OptsWT ),
     options_append_types_remove( RmvTypes, OptsWT, Opts ),
     options_append_restore_debug_status( Restore ).
@@ -217,47 +224,47 @@ options_append_args( _Opts, Args, Arity ) :-
     Arity is 1.
 */
 
-options_append_process( [], Opts, _Args, _Defs, _Pname, Opts ).
-options_append_process( [extra_arg(_)|T], All, Args, Defs, Pname, Opts ) :-
+options_append_process( [], Opts, _Args, _Defs, _Pname, _DbgMod, Opts ).
+options_append_process( [extra_arg(_)|T], All, Args, Defs, Pname, DbgMod, Opts ) :-
     !,
-    options_append_process( T, All, Args, Defs, Pname, Opts ).
-options_append_process( [process(Opt)|T], All, Args, Defs, Pname, Opts ) :-
-    !,
-    options_remainder( T, process, 1, Opt, Rem ),
-    options_append_process_option( Opt, All, Pname, Args, Defs, Nxt, _Enh, Opts ),
-    options_append_process( Rem, Nxt, Args, Defs, Pname, Opts ).
-options_append_process( [funnel(Opt)|T], All, Args, Defs, Pname, Opts ) :-
+    options_append_process( T, All, Args, Defs, Pname, DbgMod, Opts ).
+options_append_process( [process(Opt)|T], All, Args, Defs, Pname, DbgMod, Opts ) :-
     !,
     options_remainder( T, process, 1, Opt, Rem ),
-    options_append_process_option( Opt, All, Pname, Args, Defs, _Nxt, Enh, Rem ),
-    options_append_process( Rem, Enh, Args, Defs, Pname, Opts ).
-options_append_process( [foreign(Fgn)|T], All, Args, Defs, Pname, Opts ) :-
+    options_append_process_option( Opt, All, Pname, DbgMod, Args, Defs, Nxt, _Enh, Rem ),
+    options_append_process( Rem, Nxt, Args, Defs, Pname, DbgMod, Opts ).
+options_append_process( [funnel(Opt)|T], All, Args, Defs, Pname, DbgMod, Opts ) :-
+    !,
+    options_remainder( T, process, 1, Opt, Rem ),
+    options_append_process_option( Opt, All, Pname, DbgMod, Args, Defs, _Nxt, Enh, Rem ),
+    options_append_process( Rem, Enh, Args, Defs, Pname, DbgMod, Opts ).
+options_append_process( [foreign(Fgn)|T], All, Args, Defs, Pname, DbgMod, Opts ) :-
     !,
     options_remainder( T, process, 1, Fgn, Rem ),
     exclude( template_in_defaults(Defs), All, Fgn ),
-    options_append_process( Rem, All, Args, Defs, Pname, Opts ).
-options_append_process( [Opt|_T], _All, _Args, _Defs, Pname, _Opts ) :-
-    throw( unknown_option_in_options_append(Opt,Pname) ). % fixme
+    options_append_process( Rem, All, Args, Defs, Pname, DbgMod, Opts ).
+options_append_process( [Opt|_T], _All, _Args, _Defs, Pname, DbgMod, _Opts ) :-
+    throw( unknown_option_in_options_append(Opt,Pname,DbgMod) ). % fixme
 
 template_in_defaults( Defs, Term ) :-
     functor( Term, Tname, Tarity ),
     functor( Template, Tname, Tarity ),
     memberchk( Template, Defs ).
 
-options_append_process_option( Opt, All, Pname, Args, Defs, Nxt, Enh, Opts ) :-
+options_append_process_option( Opt, All, Pname, DbgMod, Args, Defs, Nxt, Enh, Rem ) :-
     options_append_known_process_option( Opt ),
     !,
-    options_append_option_process( Opt, All, Pname, Args, Defs, Nxt, Enh, Opts ).
-options_append_process_option( Opt, _All, _Pname, _Args, _Defs, _Nxt, _Enh, _Opts ) :-
+    options_append_option_process( Opt, All, Pname, DbgMod, Args, Defs, Nxt, Enh, Rem ).
+options_append_process_option( Opt, _All, _Pname, _DbgMod, _Args, _Defs, _Nxt, _Enh, _Rem ) :-
     throw( options_append( unknown_process_option(Opt)) ).
 
 % user + program can use debug/0,1,2 the first one
 % is only used, the Dbg terms argument can be a list if
 % more subjects need debugging
 %
-% fixme: this doesn't work properly from multi_debugs
+% fixme: this doesn't work properly for multi_debugs
 %
-options_append_option_process( debug, All, Pname, _Args, _Defs, NxtEnh, Enh, _Opts ) :-
+options_append_option_process( debug, All, Pname, DbgMod, _Args, _Defs, NxtEnh, Enh, _Rem ) :-
     partition( option_name(debug), All, Dbgs, Nxt ),
     Dbgs = [Dbg|_],
     !,
@@ -267,13 +274,13 @@ options_append_option_process( debug, All, Pname, _Args, _Defs, NxtEnh, Enh, _Op
     Rst = '$restore'(Pname,debug,Status),
     Enh = [Rst|All],
     NxtEnh = [Rst|Nxt],
-    options_append_option_process_debug( Dbg, Pname ).
+    options_append_option_process_debug( Dbg, Pname, DbgMod ).
 % next clause states that we shouldn't complain if there is no debug/1,2,3
-options_append_option_process( debug, All, _Pname, _Args, _Defs, Nxt, Enh, _Opts ) :-
+options_append_option_process( debug, All, _Pname, _DbgMod, _Args, _Defs, Nxt, Enh, _Rem ) :-
     % fixme: add option_append option for strictness here ?
     Nxt = All,
     Enh = All.
-options_append_option_process( version, All, Pname, Args, Defs, NxtEnh, Enh, _Opts ) :-
+options_append_option_process( version, All, Pname, _DbgMod, Args, Defs, NxtEnh, Enh, _Opts ) :-
     ( memberchk(version(V),Defs) -> true; V=null ),
     ( memberchk(version(RetV),Args) ->
         ThisV =.. [Pname,V],
@@ -308,23 +315,23 @@ options_append_option_process_version_return_list( Var, ThisV ) :-
 options_append_option_process_version_return_list( [_H|T], ThisV ) :-
     options_append_option_process_version_return_list( T, ThisV ).
 
-options_append_option_process_debug( debug, Pname ) :-
+options_append_option_process_debug( debug, Pname, Mod ) :-
     options_append_option_process_debug_register( Pname ),
-    options_append_option_process_debug_arg( Pname, true ).
-options_append_option_process_debug( debug(DbgS), Pname ) :-
+    options_append_option_process_debug_arg( Pname, Mod, true ).
+options_append_option_process_debug( debug(DbgS), Pname, Mod ) :-
     options_en_list( DbgS, Dbgs ),
     options_append_option_process_debug_register( Pname ),
-    maplist( options_append_option_process_debug_arg(Pname), Dbgs ).
-options_append_option_process_debug( debug(DbgS,Prior), Pname ) :-  % fixme, this is now dealt by '$restore' ?
+    maplist( options_append_option_process_debug_arg(Pname,Mod), Dbgs ).
+options_append_option_process_debug( debug(DbgS,Prior), Pname, Mod ) :-  % fixme, this is now dealt by '$restore' ?
     options_en_list( DbgS, Dbgs ),
     options_append_option_process_debug_register( Pname ),
     debugging_status( Pname, Prior ),
-    maplist( options_append_option_process_debug_arg(Pname), Dbgs ).
+    maplist( options_append_option_process_debug_arg(Pname,Mod), Dbgs ).
 
 options_append_option_process_debug_register( Pname ) :-
     ( prolog_debug:debugging(Pname,_,_) -> true; assert(prolog_debug:debugging(Pname,false,[user])) ).
 
-options_append_option_process_debug_arg( _, Var ) :-
+options_append_option_process_debug_arg( _, _, Var ) :-
     var( Var ),
     !,
     throw( options_append(instantiation_of_debug) ).
@@ -332,17 +339,20 @@ options_append_option_process_debug_arg( _, Var ) :-
 options_append_option_process_debug_arg( Pname, _ ) :- !,
     \+ prolog_debug:debug_topic(Pname).
     */
-options_append_option_process_debug_arg( _Pname, none ) :- !.
-options_append_option_process_debug_arg( Pname, false ) :- !,
+options_append_option_process_debug_arg( _Pname, _Mod, none ) :- !.
+options_append_option_process_debug_arg( Pname, _Mod, false ) :- !,
     nodebug( Pname ).
-options_append_option_process_debug_arg( Pname, true ) :- !,
-    ( prolog_debug:debug_topic(Pname) -> true; prolog_debug:debug_topic(Pname) ),
-    debug( options_append, 'Turning debugging on for predicate handle: ~w', [Pname] ),
-    debug( Pname ).
-options_append_option_process_debug_arg( _Pname, all ) :- !,
-    debug( options_append, 'Turning indescriminate debugging on', [] ),
-    debug( _ ).
-options_append_option_process_debug_arg( _, Other ) :- !,
+options_append_option_process_debug_arg( Pname, Mod, true ) :- !,
+    ( Mod == false -> Pterm = Pname; Pterm =.. [Mod,Pname] ),
+    ( prolog_debug:debug_topic(Pterm) -> true; prolog_debug:debug_topic(Pterm) ),
+    debug( options_append, 'Turning debugging on for predicate handle: ~w', [Pterm] ),
+    debug( Pterm ).
+options_append_option_process_debug_arg( _Pname, Mod, all ) :- !,
+    ( Mod == false -> Pterm = _ ; Pterm =.. [Mod,_] ),
+    debug( options_append, 'Turning debugging on for generic handle: ~w', [Pterm] ),
+    debug( Pterm ).
+options_append_option_process_debug_arg( _, _Mod, Other ) :- !,
+    % fixme: should we enforce Mod here ? seems it better not to...
     debug( options_append, 'Turning debugging on for handle: ~w', [Other] ),
     debug( Other ).
 
