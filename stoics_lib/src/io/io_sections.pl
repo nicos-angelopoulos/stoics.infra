b0aa9cec06f7c08a411c027329e94a312bff5923
Nicos Angelopoulos <nicos@gmx.co.uk>
2021-05-09 20:19:04 +0100

    NEW:    created directory examples/ EXAMPL: examples/sectioned.txt for io_sections/3 (and new doc comments in predicate) DOC:    option separator_call(G)@io_sections/3 IFACE:  pass first arg of io_sections/3 via absolute_file_name/3, before open/3 OPTS:   include_separator(Inc)@io_sections/3

--- stoics_lib/src/io/io_sections.pl
+++ stoics_lib/src/io/io_sections.pl
@@ -1,9 +1,15 @@
 
 :- use_module(library(readutil)).   % read_line_to_codes/2.
 
-io_sections_defaults( [separator(`//`),process(=),process_options(false),
-                       terminating_separator(true),
-				   separator_id(false)] ).
+io_sections_defaults( Defs ) :-
+    Defs = [   
+                include_separator(false),
+                process(=),
+                process_options(false),
+                separator(`//`),
+		        separator_id(false),
+                terminating_separator(true)
+    ].
 
 /** io_sections( +File, -Sections, +Opts ).
 
@@ -12,6 +18,10 @@ In vanilla operation, each section is a list of the codes read-in.
 Each section is delimited by a marker line. 
 
 Opts 
+  * include_separator(Inc=false)
+    whether to include to the separating line. (Terminator, is never retruned.)
+    See example 2.
+
   * process(Pgoal= (=))
     Goal to process the Sections before storing.
 
@@ -19,7 +29,9 @@ Opts
     else pass Sopts to processor Pgoal (as last arg)
 
   * separator_call(SepCall)
-    if given it is used to separate sections
+    if given it is used to separate sections, called with 1 argument:
+    the current line. If the call succeeds, the line is considered to be a 
+    separator line. See example 3.
 
   * separator_id(Sid=false)
     if true SepCall is called with an extra argument which is 
@@ -33,6 +45,19 @@ Opts
     whether a terminating separator is required at end of file
 
 ==
+ ?- write('example 1'), nl.
+ ?- io_sections( pack('stoics_lib/examples/sectioned.txt'), Sects, separator(`[term]`) ).
+ Sects = [[[97], [98]], [[99], [100]]].
+
+ ?- write('example 2'), nl.
+ ?- o_sections( pack('stoics_lib/examples/sectioned.txt'), Sects, [separator(`[term]`),include_separator(true)] ).
+    Sects = [[[91, 116, 101, 114, 109, 93], [97], [98]], [[91, 116, 101, 114, 109, 93], [99], [100]]].
+
+
+ ?- write('example 3'), nl.
+ ?- 
+
+ ?- write('private example'), nl.
  ?- cd( '/usr/local/users/nicos/work/2015/15.10.05-lmtk3_substrates' ).
  ?- io_sections( 'uniprot_sprot.dat', Sects, process(length) ).
 
@@ -40,6 +65,7 @@ Opts
 @author nicos angelopoulos
 @version  0.1 2015/10/05
 @version  0.2 2016/02/04
+@version  0.3 2021/02/04, added include_separator(Inc), examples, pass file through absolute_file_name/2
 
 */
 io_sections( File, Sections, ArgS ) :-
@@ -61,27 +87,34 @@ io_sections( File, Sections, ArgS ) :-
     memberchk( separator(Sep), Opts ),
     memberchk( process(Proc), Opts ),
     memberchk( process_options(Popts), Opts ),
+    memberchk( include_separator(Inc), Opts ),
 
-	open( File, read, Stream ),
+    absolute_file_name( File, FileAbs ),
+	open( FileAbs, read, Stream ),
 	read_line_to_codes( Stream, Line ),
 	% options( terminating_separator(Tmn), Opts ),
     memberchk( terminating_separator(Tmn), Opts ),
-	io_sections_stream( Line, Stream, Call, Popts, Proc, Tmn, Sid, null, [], Sections ),
+	io_sections_stream( Line, Stream, Call, Popts, Proc, Tmn, Sid, Inc, null, [], Sections ),
 	close( Stream ).
 
-io_sections_stream( end_of_file, _Stream, _Call, Popts, Proc, Tmn, Sid, Id, Acc, Sections ) :-
+io_sections_stream( end_of_file, _Stream, _Call, Popts, Proc, Tmn, Sid, Inc, Id, Acc, Sections ) :-
 	!,
-	io_sections_end_acc( Tmn, Sid, Id, Popts, Proc, Acc, Sections ).
-io_sections_stream( Line, Stream, Call, Popts, Proc, Tmn, Sid, Id, Acc, Sections ) :-
+	io_sections_end_acc( Tmn, Sid, Inc, Id, Popts, Proc, Acc, Sections ).
+io_sections_stream( Line, Stream, Call, Popts, Proc, Tmn, Sid, Inc, Id, Acc, Sections ) :-
 	% Line == Sep,
 	io_section_sep_call( Sid, Call, Line, NxtId ),
 	!,
 	io_accumulator_section( Acc, Sid, Id, Popts, Proc, Sections, Tections ),
+    ( Inc == true -> 
+        NewAcc = [Line]
+        ;
+        NewAcc = []
+    ),
 	read_line_to_codes( Stream, New ),
-	io_sections_stream( New, Stream, Call, Popts, Proc, Tmn, Sid, NxtId, [], Tections ).
-io_sections_stream( Line, Stream, Call, Popts, Proc, Tmn, Sid, Id, Acc, Sections ) :-
+	io_sections_stream( New, Stream, Call, Popts, Proc, Tmn, Sid, Inc, NxtId, NewAcc, Tections ).
+io_sections_stream( Line, Stream, Call, Popts, Proc, Tmn, Sid, Inc, Id, Acc, Sections ) :-
 	read_line_to_codes( Stream, New ),
-	io_sections_stream( New, Stream, Call, Popts, Proc, Tmn, Sid, Id, [Line|Acc], Sections ).
+	io_sections_stream( New, Stream, Call, Popts, Proc, Tmn, Sid, Inc, Id, [Line|Acc], Sections ).
 
 io_section_sep_call( true, Call, Line, Id ) :-
 	call( Call, Line, Id ).
@@ -106,12 +139,14 @@ optioned_call( false, Proc, Provisional, Section ) :-
 optioned_call( Popts, Proc, Provisional, Section ) :-
 	call( Proc, Provisional, Section, Popts ).
 
-io_sections_end_acc( [] ) :- !.
-io_sections_end_acc( Acc ) :- 
+io_sections_end_acc( [], _Inc ) :- !.  % should be Inc == false...
+io_sections_end_acc( [_A], true ) :- 
+    !.
+io_sections_end_acc( Acc, true ) :- 
 	% fixme: use pack(pack_errors)
 	throw( non_empty_end_accumulator(Acc) ).
 
-io_sections_end_acc( true, _Sid, _Id, _Popts, _Proc, Acc, [] ) :-
-	io_sections_end_acc( Acc ).
-io_sections_end_acc( false, Sid, Id, Popts, Proc, Acc, Sections ) :-
+io_sections_end_acc( true, _Sid, Inc, _Id, _Popts, _Proc, Acc, [] ) :-
+	io_sections_end_acc( Acc, Inc ).
+io_sections_end_acc( false, Sid, _Inc, Id, Popts, Proc, Acc, Sections ) :-
 	io_accumulator_section( Acc, Sid, Id, Popts, Proc, Sections, [] ).
