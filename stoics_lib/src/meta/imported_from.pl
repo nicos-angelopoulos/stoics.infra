6d43e6b4c8a414166442a934b618728854398a5b
Nicos Angelopoulos <nicos@gmx.co.uk>
2022-02-05 20:38:41 +0000

    IFACE:  imported_from/2 look in user:Goal if failed to find the property for Goal

--- stoics_lib/src/meta/imported_from.pl
+++ stoics_lib/src/meta/imported_from.pl
@@ -1,11 +1,11 @@
 /** imported_from( +Clauser, +Mod ).
 
 Holds if Goal corresponding to Clauser (a goal or predicate identifier)
-hs predicate_property/2 defined property imported_from(Mod), else
-Mod is user.
+and predicate_property/2 defines property imported_from(Mod), else Mod is user.
 
 @author nicos angelopoulos
 @version  0.1 2017/2/22
+@version  0.2 2022/2/05,  try user:Goal if failed on G
 
 */
 imported_from( Pname/Parity, Mod ) :-
@@ -14,5 +14,8 @@ imported_from( Pname/Parity, Mod ) :-
     imported_from( Goal, Mod ).
 imported_from( Goal, Mod ) :-
     predicate_property( Goal, imported_from(Mod) ),
+    !.
+imported_from( Goal, Mod ) :-
+    predicate_property( user:Goal, imported_from(Mod) ),
     !.
 imported_from( _Goal, user ).
