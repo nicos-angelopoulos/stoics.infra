2d5890853512c16e3309a4df650e9bed4e638b7e
Nicos Angelopoulos <nicos@gmx.co.uk>
2022-06-14 12:14:05 +0100

    VERS:   1.6

--- os_lib/prolog/os_lib.pl
+++ os_lib/prolog/os_lib.pl
@@ -132,7 +132,6 @@ Common options:
   * idir(Idir='.')
     directory for input (overrides Dir)
 
-
   * odir(Odir='.')
     directory for output (overrides Dir)
 
@@ -228,6 +227,7 @@ The library predicates can be split to 4 groups.
 @version 1.3 2018/10/1 cleaner error handling via throw, new opt dots(D), os_cast/3 arguments switch
 @version 1.4 2019/4/22 option sub(Sub); cp_rec.pl script; list of postfixes, etc
 @version 1.5 2019/4/22 os_path/2, fixes and new options to os_mill/4 ; os_exists/2 (return type) & os_sel/4
+@version 1.6 2022/6/14 fixed: os_exist( file, type(link) ), added option read_link(RLnk) to os_file/2
 @see http://www.stoics.org.uk/~nicos/sware/os
 @see http://www.stoics.org.uk/~nicos/sware/os/html/os_lib.html
 @see doc/Releases.txt
@@ -291,13 +291,13 @@ The library predicates can be split to 4 groups.
 
 /** os_version( -Version, -Date ).
 
-Current version and release date for the library.
+Current version and release date for the pack.
 
 ==
-?- os_version( V, D ) :-
-    V = 1:5:0,
-    D = date(2020,9,18)
+?- os_version( V, D ).
+V = 1:6:0,
+D = date(2022,6,14)
 ==
 
 */
-os_version( 1:5:1, date(2020,9,18) ).
+os_version( 1:6:0, date(2022,6,14) ).
