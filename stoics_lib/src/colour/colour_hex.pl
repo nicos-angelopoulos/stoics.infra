c6b60f6685010bed416d86925d51f538e36365e5
Nicos Angelopoulos <nicos@gmx.co.uk>
2022-12-16 12:35:51 +0000

    DOC:   added examples and more text

--- stoics_lib/src/colour/colour_hex.pl
+++ stoics_lib/src/colour/colour_hex.pl
@@ -1,19 +1,32 @@
-/** bio_colour_hex( Clr, Hex ).
+/** colour_hex( +Clr, -Hex ).
 
-Convert input Clr to a hex 
+Convert lexical input Clr to a hex string.
 
-Clr is passed through lexi as is &(Hex).
+Clr is passed through lexi/2.
 
-Currently passes through any represeantion of a hex and 
-the long or code names from bio_latex_colour/4.
+Currently passes through any represeantion of a hex and maps
+the long or code names from latex_colour/4 (1st and 3rd argument to second argument).
 
 ==
-==
+?- colour_hex( amber, Hex ).
+Hex = "#FFBF00".
 
+?- colour_hex( amberic, Hex ).
+ERROR: colour_hex/2: Cannot find colour: amberic
+false.
+
+?- colour_hex( '#FFBB00', Hex ).
+Hex = "#FFBB00".
+
+?- colour_hex( '#FFBB00wrong', Hex ).
+Hex = "#FFBB00wrong".
+==
 
 @author nicos angelopoulos
 @version  0.1 2022/12/16
-@see bio_volcano_plot/1
+@see bio_analytics:bio_volcano_plot/1
+@see lexi/2
+@tbd do some basic checks if a Hex is given
 
 */
 colour_hex( Clr, Hex ) :-
@@ -25,9 +38,8 @@ colour_hex_atom( ClrAtm, Hex ) :-
      !,
      lexi( ClrAtm, &(Hex) ).
 colour_hex_atom( ClrAtm, Hex ) :-
-     latex_colour( ClrAtm, HexAtm, _, _ ),
-     !,
-     lexi( HexAtm, &(Hex) ).
+     latex_colour( ClrAtm, Hex, _, _ ),
+     !.
 colour_hex_atom( ClrAtm, Hex ) :-
      latex_colour( _, HexAtm, ClrAtm, _ ),
      !,
