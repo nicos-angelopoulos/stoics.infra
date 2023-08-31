dbea877e0311acc66a29c412b6b5f77f774fa526
Nicos Angelopoulos <nicos@gmx.co.uk>
2023-08-31 11:27:31 +0100

    IFACE:  allow atomic first argument in compound/3

--- stoics_lib/src/term/compound.pl
+++ stoics_lib/src/term/compound.pl
@@ -1,33 +1,40 @@
 %% compound( +Term, -Name, -Args ).
 %
-%  Tries to deal with syntax changes that allow a() as a legal term.
+%  Generalises =.. and compound_name_arguments/3 in that it also allows atomic Term.
 %
-% Examples run on Swi.7 
+% v0.2 allows atomic Term, which breaks backward compatibility (previously these will fail).
+%
+% Examples
 %==
-%  compound( abc, Name, Args ).
-%  false.
+% ?- compound( abc, Name, Args ).
+% Name = abc,
+% Args = [].
 %  
-%  compound( abc(a,b,c), Name, Args ).
+% ?- compound(abc(a,b,c), Name, Args).
 % Name = abc,
 % Args = [a, b, c].
 % 
-% compound( Term, abc, [a,b,c] ).
+% ?- compound( Term, abc, [a,b,c] ).
 % Term = abc(a, b, c).
 % 
-% compound( Term, abc, [] ).
+% ?- compound( Term, abc, [] ).
 % Term = abc().
 %
 %==
 %
 % @author nicos angelopoulos
-% @version  0.1 2014/1/10 (round about)
+% @version  0.1 2014/1/10 ~
+% @version  0.2 2023/8/31, allows atomic Term
 %
 compound( Term, Name, Args ) :-
 	current_predicate( compound_name_arguments/3 ),
 	!,
-	once( (compound(Term) ; (ground(Name),is_list(Args))) ),
-	% !,
-	compound_name_arguments( Term, Name, Args ).
+	( (compound(Term) ; (ground(Name),is_list(Args))) ->
+	     compound_name_arguments( Term, Name, Args )
+          ;
+          atomic(Term),
+          Name = Term, Args = []
+     ).
 compound( Term, Name, Args ) :-
 	once( (compound(Term) ; (ground(Name),ground(Args))) ),
 	Term =.. [Name,Args].
