bec4444813d17d8a0dc06fb22b6097372df6758c
Nicos Angelopoulos <nicos@gmx.co.uk>
2023-09-20 12:25:10 +0100

    IFACE:  1st arg of arg_add/4 can now be a negative integer

--- stoics_lib/src/term/arg_add.pl
+++ stoics_lib/src/term/arg_add.pl
@@ -14,12 +14,18 @@ X = x(4, 2, 3, 1).
 ?- arg_add( L, x(1,2,3), [4,5], Five ).
 Five = x(1, 2, 3, 4, 5).
 
+?- arg_add( 3, a(a,b,d,e), c, New ).
+New = a(a, b, c, d, e).
+
+?- arg_add( -3, a(a,b,d,e), c, New ).
+New = a(a, b, c, d, e).
 ==
 
 @author nicos angelopoulos
 @version  0.1 2016/10/28
 @version  0.2 2018/12/4,  add expressions for N, added to stoics_lib
-@version  0.3 2022/12/21, Arg can be a list now and Nin a var. examples.
+@version  0.3 2022/12/21, Arg can be a list now and N a variable. added examples
+@version  0.4 2023/09/20, N can be a negative integer
 
 */
 arg_add( Nin, Term, Arg, New ) :-
@@ -28,7 +34,12 @@ arg_add( Nin, Term, Arg, New ) :-
           length( Args, M ),
           N is M + 1
           ;
-          N is Nin
+          ( Nin < 0 ->
+               functor( Term, _, TermArity ),
+               N is TermArity + Nin + 2
+               ;
+               N is Nin
+          )
     ),
     M is N - 1,
     length( Prefix, M ),
