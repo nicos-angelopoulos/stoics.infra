4967bde8fa879b4de028a1f1829a2a7a04a9ed7a
Nicos Angelopoulos <nicos@gmx.co.uk>
2024-09-06 16:37:48 +0100

    OPTS:   url_file/3, added dnt(csv) & true/pl; hash(); extract(); dn_dir()

--- stoics_lib/src/web/url_file.pl
+++ stoics_lib/src/web/url_file.pl
@@ -1,14 +1,31 @@
 
-:- use_module(library(debug)).  % /3.
+:- use_module(library(debug)).          % /3.
+:- use_module(library(process)).        % process_create/3.
+:- use_module(library(readutil)).       % read_line_to_codes/2.
 :- use_module(library(http/http_open)). % fixme: doit dynamically
 :- use_module(library(listing)).        % portray_clause/2.
-:- use_module(library(socket)).         % gethostname/1
+:- use_module(library(socket)).         % gethostname/1.
 
 :- lib(at_con/3).
 :- lib(get_datetime/1).
 :- lib(suggests(options)).
 
-url_file_defaults( [overwrite(error),dnt(false),iface(prolog),insecure(false)] ).
+url_file_extract( gz, gunzip, ['-k'] ).
+
+url_file_defaults( Defs ) :-
+                         Defs  =  [ 
+                                     dn_dir(Downloads),
+                                     dnt(false),
+                                     hash(Hasher),
+                                     extract(Xtc),
+                                     iface(prolog),
+                                     insecure(false),
+                                     overwrite(error)
+                                  ],
+        ( user:file_search_path(downloads,Downloads) -> true; Downloads = '.' ),
+        DefHasher = sha256sum,
+        ( catch(process_create(path(which),[DefHasher],[stdout(null)]),_,fail) -> DefHasher = Hasher ; Hasher = false ),
+        ( catch(process_create(path(which),[gunzip],[stdout(null)]),_,fail) -> Xtc = true ; Xtc = false ).
 
 %% url_file( +Url, ?File ).
 %% url_file( +Url, ?File, +Opts ).
@@ -23,9 +40,17 @@ url_file_defaults( [overwrite(error),dnt(false),iface(prolog),insecure(false)] )
 % The main download code is a copy-paste from SWI's library(prolog_pack) file.
 % 
 % Opts 
+% * dn_dir(DnDir)
+%   downloads direcotry. Def is =|user:file_search_path(downloads,DnDir)|= if it exists and _./_ otherwise.
 % * dnt(Dnt=false)
-%   if true, create a File.dnt with the start and end datime/6 stamps.
-%   From v0.4 also records the Url and the download host.
+%   if _pl_ or _true_ creates a File.dnt with the start and end datime/6 term stamps (v0.4: wrapped in start(), end()).<br>
+%   if _csv_ creates File.csv with the same info as dnt but in csv format (2 columns)
+%   From v0.4 also records the Url, the download host and hash value for the download (see hash())
+% * hash(Hash=sha256sum)
+%   executable for creating a hash which is registered in .dnt or .csv file (dnt()). Use _false_ for no hash, and no relavant entry.<br>
+%   Default is _sha256sum_ if the executable is in PATH other wise false.
+% * extract(Xtc)
+%   whether to uncomompress/extract file if download has recognisable compressed extension (_true_ if gunzip in PATH; _false_ otherwise)
 % * iface(Iface=prolog)
 %   or _wget_
 % * insecure(Insec=false)
@@ -60,62 +85,62 @@ url_file_defaults( [overwrite(error),dnt(false),iface(prolog),insecure(false)] )
 % @version  0.1 2014/07/23
 % @version  0.2 2015/11/24  added option overwrite/1
 % @version  0.3 2018/03/13, removed url_file/1 but url_file/2 allows -File, moved to pack(stoics_lib)
+% @version  0.4 2024/09/06, added dnt(csv) & true/pl; hash(); extract(); dn_dir()
 %
 url_file( Url, LocP ) :-
-    url_file( Url, LocP, [] ).
-
-url_file( Url, LocP, Opts ) :-
-    var(LocP),
-	user:file_search_path( downloads, Downloads ),
-	!,
-	make_directory_path( Downloads ),
-	file_base_name( Url, RemB ),
-	directory_file_path( Downloads, RemB, LocP ),
-	url_file( Url, LocP, Opts ).
+     url_file( Url, LocP, [] ).
 url_file( Url, RemB, Opts ) :-
-    var(RemB),
-    !,
-	file_base_name( Url, RemB ),
-	url_file( Url, RemB, Opts ).
+     var(RemB),
+     !,
+     options( dn_dir(Dnloads), Opts ),
+     file_base_name( Url, RemB ),
+     ( Dnloads = '.' -> 
+          RemB = LocP
+          ;
+          make_directory_path( Dnloads ),
+          directory_file_path( Dnloads, RemB, LocP )
+     ),
+     url_file( Url, LocP, Opts ).
 url_file( Url, Local, Args ) :-
-	options_append( url_file, Args, Opts ),
-	options( overwrite(Ow), Opts ),
+     options_append( url_file, Args, Opts ),
+     options( overwrite(Ow), Opts ),
      options( iface(Iface), Opts ),
-	url_file_ow( Ow, Iface, Url, Local, Opts ).
+     url_file_ow( Ow, Iface, Url, Local, Opts ).
 
 url_file_ow( false, _Iface, Url, Local, _Opts ) :- 
-	exists_file( Local ),
-	!,
-	debug( url_file, 'Local file exists: ~p, not downloading it again from: ~p.', [Local,Url] ),
+     exists_file( Local ),
+     !,
+     debug( url_file, 'Local file exists: ~p, not downloading it again from: ~p.', [Local,Url] ),
     fail.
 url_file_ow( fail, _Iface, Url, Local, _Opts ) :- 
-	exists_file( Local ),
-	!,
-	debug( url_file, 'Local file exists: ~p, not downloading it again from: ~p.', [Local,Url] ),
+     exists_file( Local ),
+     !,
+     debug( url_file, 'Local file exists: ~p, not downloading it again from: ~p.', [Local,Url] ),
     fail.
 url_file_ow( error, _Iface, Url, Local, _Opts ) :-
-	exists_file( Local ),
-	!,
-	throw( refusing_to_download_url_to_existing_file(Local,Url) ).
+     exists_file( Local ),
+     !,
+     throw( refusing_to_download_url_to_existing_file(Local,Url) ).
 url_file_ow( _, Iface, Url, Local, Opts ) :- % fixme, add error value for ow() ?
-	debug( url_file, 'Downloading URL: ~p, onto file: ~p, with interface: ~w', [Url,Local,Iface] ),
-	get_datetime( StartDt ),
+     debug( url_file, 'Downloading URL: ~p, onto file: ~p, with interface: ~w', [Url,Local,Iface] ),
+     get_datetime( StartDt ),
      options( insecure(Insec), Opts ),
      url_file_dnload( Iface, Insec, Url, Local ),
-	get_datetime( EndDt ),
-	options( dnt(Dnt), Opts ),
-	url_file_dnt( Dnt, Local, StartDt, EndDt, Url ).
+     get_datetime( EndDt ),
+     options( dnt(Dnt), Opts ),
+     ( Dnt == csv -> use_module(library(csv)); true ),
+     url_file_dnt( Dnt, Local, StartDt, EndDt, Url, Opts ).
 
 url_file_dnload( prolog, Insec, Url, Local ) :-
      ( Insec == true -> OpenOpts = [cert_verify_hook(ssl_verify_null)] ; OpenOpts = [] ),
-	setup_call_cleanup(
-	    http_open(Url, In, OpenOpts),
-	    setup_call_cleanup(
-	    open(Local, write, Out, [type(binary)]),
-	    copy_stream_data(In, Out),
-	    close(Out)),
-	    close(In)
-	).
+     setup_call_cleanup(
+         http_open(Url, In, OpenOpts),
+         setup_call_cleanup(
+         open(Local, write, Out, [type(binary)]),
+         copy_stream_data(In, Out),
+         close(Out)),
+         close(In)
+     ).
 url_file_dnload( wget, Insec, Url, Local ) :-
      ( Insec == true -> 
                     at_con(['wget --no-check-certificate -O',Local,Url], ' ', Wget) 
@@ -124,19 +149,70 @@ url_file_dnload( wget, Insec, Url, Local ) :-
      ),
      shell( Wget ).
 
-url_file_dnt( true, Local, StartDt, EndDt, Url ) :-
-	file_name_extension( Local, dnt, DntF ),
+url_file_dnt( false, _Local, _StartDt, _EndDt, _Url, _Opts ) :- !.
+url_file_dnt( RecAsPrv, Local, StartDt, EndDt, Url, Opts ) :-
+     ( RecAsPrv == csv -> 
+          RecAs = csv,
+          file_name_extension( Local, dnv, DntF )
+          ;
+          RecAs = pl,
+          file_name_extension( Local, dnt, DntF )
+     ),
      gethostname( Hname ),
-	% os_ext( dnt, Local, DntF ),
-	open( DntF, write, Out ),
-	portray_clause( Out, StartDt ),
-	portray_clause( Out, EndDt ),
-     portray_clause( Out, url(Url) ),
-     portray_clause( Out, dnl_host(Hname) ),
-	close( Out ).
-url_file_dnt( false, _Local, _StartDt, _EndDt, _Url ).
+     % os_ext( dnt, Local, DntF ),
+     open( DntF, write, Out ),
+     url_file_dnt_rec( RecAs, Out, start(StartDt) ),
+     url_file_dnt_rec( RecAs, Out, end(EndDt) ),
+     url_file_dnt_rec( RecAs, Out, url(Url) ),
+     url_file_dnt_rec( RecAs, Out, dnl_host(Hname) ),
+     options( hash(Hopt), Opts ),
+     url_file_dnt_hash( Hopt, Local, RecAs, Out ),
+     options( extract(Xtc), Opts ),
+     url_file_dnt_extract( Xtc, Hopt, Local, RecAs, Out ),
+     close( Out ).
 
+url_file_dnt_extract( true, Hopt, Local, RecAs, Out ) :-
+     file_name_extension( Base, Ext, Local ),
+     ( url_file_extract(Ext,Exec,Args) -> 
+                    append( Args, [Local], Alls ),
+                    process_create(path(Exec),Alls,[]),
+                    url_file_dnt_hash( Hopt, Base, RecAs, Out )
+                    ;
+                    true
+     ).
+url_file_dnt_extract( false, _Hopt, _Local, _RecAs, _Out ).
 
+url_file_dnt_rec( csv, Out, Term ) :-
+     Term =.. [Key|Args],
+     ( (Key==start;Key==end) ->
+          Args = [datetime(Y,M,D,Hr,Mn,Sc)],
+          atomic_list_concat( [Y,',',M,',',D,'@',Hr,':',Mn,':',Sc], '', Narg ),
+          Nargs = [Key,Narg]
+          ;
+          Nargs = [Key|Args]
+     ),
+     length( Nargs, Nlen ),
+     Rem is 4 - Nlen,
+     findall( '', between(1,Rem,_), Pads ),
+     append( Nargs, Pads, Alls ),
+     Row =.. [row|Alls],
+     csv_write_stream( Out, [Row], [] ).
+url_file_dnt_rec( pl, Out, Term ) :-
+     portray_clause( Out, Term ).
+
+url_file_dnt_hash( false, _File, _RecAs, _Out ) :- !.
+url_file_dnt_hash( Hasher, File, RecAs, Out ) :-
+     setup_call_cleanup(
+            process_create(path(Hasher), [File],
+                           [ stdout(pipe(Piped))
+                           ]),
+               ( read_line_to_codes( Piped, Codes ),
+                 atom_codes( Atom, Codes ) ),
+            close(Piped)),
+     atomic_list_concat( Parts, ' ', Atom ),
+     once( append([Hash|_], [File], Parts) ),
+     url_file_dnt_rec( RecAs, Out, hash(Hasher,File,Hash) ).
+     
 %   ssl_verify_null(+SSL, +ProblemCert, +AllCerts, +FirstCert, +Error)
 %
 %   Copied from system library(prolog_pack.pl).
@@ -146,5 +222,4 @@ url_file_dnt( false, _Local, _StartDt, _EndDt, _Url ).
 ssl_verify_null(_SSL,
            _ProblemCertificate, _AllCertificates, _FirstCertificate,
            _Error).
-
 
