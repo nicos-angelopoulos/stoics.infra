2409339e7777dbdd79c3de52e4ace5e734314450
Nicos Angelopoulos <nicos@gmx.co.uk>
2025-12-07 00:41:43 +0000

    CHANGED:   http protocol to https

--- os_lib/pack.pl
+++ os_lib/pack.pl
@@ -1,9 +1,9 @@
 name( os_lib ).
 title( 'Operating system interaction predicates.' ).
-version( '1.7' ).
+version( '2.0' ).
 author( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
 packager( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
 maintainer( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
-home( 'http://stoics.org.uk/~nicos/sware/os_lib' ).
-download( 'http://stoics.org.uk/~nicos/sware/packs/os_lib/os_lib-*.tgz' ).
+home( 'https://stoics.org.uk/~nicos/sware/os_lib' ).
+download( 'https://stoics.org.uk/~nicos/sware/packs/os_lib/os_lib-*.tgz' ).
 requires(lib).
