282cd153b440c8e195c7ff56e877e2c251b62e08
Nicos Angelopoulos <nicos@gmx.co.uk>
2025-12-07 00:58:37 +0000

    CHANGE:   all http refs in pack.pl to https

--- debug_call/pack.pl
+++ debug_call/pack.pl
@@ -1,9 +1,9 @@
 name( debug_call ).
 title( 'Debugging with calls' ).
 version( '2.1' ).
-author( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
-packager( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
-maintainer( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
-home( 'http://stoics.org.uk/~nicos/sware/debug_call' ).
-download( 'http://stoics.org.uk/~nicos/sware/packs/debug_call/debug_call-*.tgz' ).
+author( 'Nicos Angelopoulos', 'https://stoics.org.uk/~nicos' ).
+packager( 'Nicos Angelopoulos', 'https://stoics.org.uk/~nicos' ).
+maintainer( 'Nicos Angelopoulos', 'https://stoics.org.uk/~nicos' ).
+home( 'https://stoics.org.uk/~nicos/sware/debug_call' ).
+download( 'https://stoics.org.uk/~nicos/sware/packs/debug_call/debug_call-*.tgz' ).
 requires(lib).
