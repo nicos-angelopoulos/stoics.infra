344450de18e7fa013a433357e5fe29198c94175b
Nicos Angelopoulos <nicos@gmx.co.uk>
2026-01-08 18:05:30 +0000

    CHANGE: has_at_least/3, more efficient implementation

--- stoics_lib/src/list/has_at_least.pl
+++ stoics_lib/src/list/has_at_least.pl
@@ -12,10 +12,21 @@ false.
 
 @author nicos angelopoulos
 @version  0.1 2017/1/11
+@version  0.2 2026/1/8    more efficient implementation
 
 */
 has_at_least( N, X, List ) :-
 	ground( X ),
-	findall( 1, member(X,List), Ones ),
-	length( Ones, Len ),
-	N =< Len.
+     integer( N ),
+     N > 0,
+     has_at_least_1( N, List, X ).
+
+has_at_least_1( 0, _List, _X ) :-
+     !.
+has_at_least_1( N, [H|T], X ) :-
+     ( H == X ->
+          M is N - 1
+          ;
+          M is N
+     ),
+     has_at_least_1( M, T, X ).
