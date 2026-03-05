767d1623245d1bb72f60a3b0465d97c570d2c756
Nicos Angelopoulos <nicos@gmx.co.uk>
2026-03-05 01:02:09 +0000

    LIBidx:  for pack(pack_errors) (created)

---
+++ pack_errors/src/LibIndex.pl
@@ -0,0 +1,15 @@
+% date(2026,3,5)
+
+lib_index(caught, 3, any, pack_errors, '../prolog/pack_errors').
+lib_index(defined, 2, any, pack_errors, '../prolog/pack_errors').
+lib_index(defined, 3, any, pack_errors, '../prolog/pack_errors').
+lib_index(ground, 2, any, pack_errors, '../prolog/pack_errors').
+lib_index(ground_binary, 2, any, pack_errors, '../prolog/pack_errors').
+lib_index(of_same_length, 1, any, pack_errors, '../prolog/pack_errors').
+lib_index(of_same_length, 2, any, pack_errors, '../prolog/pack_errors').
+lib_index(of_same_length, 3, any, pack_errors, '../prolog/pack_errors').
+lib_index(pack_errors, 0, any, pack_errors, '../prolog/pack_errors').
+lib_index(pack_errors_version, 2, any, pack_errors, '../prolog/pack_errors').
+lib_index(throw, 2, any, pack_errors, '../prolog/pack_errors').
+lib_index(type, 2, any, pack_errors, '../prolog/pack_errors').
+lib_index(type, 3, any, pack_errors, '../prolog/pack_errors').
