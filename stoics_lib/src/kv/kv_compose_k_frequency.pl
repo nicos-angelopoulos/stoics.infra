5784ba76bad42687031a0d267e83475f8a21b9a8
Nicos Angelopoulos <nicos@gmx.co.uk>
2026-03-07 12:58:29 +0000

    DOC:   updated all examples in pred description

--- stoics_lib/src/kv/kv_compose_k_frequency.pl
+++ stoics_lib/src/kv/kv_compose_k_frequency.pl
@@ -25,8 +25,9 @@ Aggregate  Key-Value terms KVs, on Frequency of Keys.
 
 The resulting FKVs are of the form F-K-Vs, where Vs is a list of V elements.
 FKVs are returned in sorted order for K as per keysort/2 and Vs are 
-in the order they appear in KVs as keysort/2 does not use them for sorting.
-The predicate uses a first pass through keysort/2 or sort/4 before it walks through the input KVs.
+in the order they appear in KVs as keysort/2 does not use them for sorting.  
+The predicate uses a first pass through keysort/2 (or sort/4 if non K-V input is given, see below),
+before it walks through the input KVs.
 It can be used to return the groups only (see option drop_f(DropF)).
 
 kv_compose_k_frequency/3 requires pack(options), whereas kv_compose_k_frequency/2 
@@ -40,7 +41,7 @@ Opts
   * drop_v(DropV=false)
     whether to drop Vs from answer
   * keysort(Ksort=true)
-    whether to use keysort, instead of sort/2
+    whether to use keysort, (otherwise sort/2 is used creating total order)
   * ki(Kindex=1)
     the argument position for Keys
   * vi(Vindex=2)
@@ -60,41 +61,32 @@ FKVo = [1-2-b, 2-1-[x, a]].
 kv_compose_k_frequency/3 examples
 ==
 ?- kv_compose_k_frequency([2-b,1-x,1-a], FKVs, []). 
-FKVs = [2-1-[x, a], 1-2-b].
+FKVs = [2-1-[x, a], 1-2-[b]].
 
 ?- kv_compose_k_frequency([2-b,1-x,1-a], FKVs, keysort(false)).
-FKVs = [2-1-[a, x], 1-2-b].
+FKVs = [2-1-[a, x], 1-2-[b]].
 
-?- kv_compose_k_frequency([2-b,1-x,1-a], FKVs, [drop_k(true)]). 
-FKVs = [2-[x, a], 1-b].
 
-?- kv_compose_k_frequency([2-b,1-x,1-a], FKVs, [drop_v(true)]).
+?- kv_compose_k_frequency([2-b,1-x,1-a], FKVs, drop_k(true)). 
+FKVs = [2-[x, a], 1-[b]].
+
+?- kv_compose_k_frequency([2-b,1-x,1-a], FKVs, drop_v(true)).
 FKVs = [2-1, 1-2].
 
 ?- kv_compose_k_frequency([2-b,1-x,1-a], FKVs, [drop_k(true),drop_v(true)]).
 FKVs = [2, 1].
 
 ?- lib(stoics_lib:kv_compose_k_nth/3).
-?- kv_compose_k_nth([no(1,2,3),no(4,2,6),no(7,8,9)], 2, KVs), kv_compose_k_frequency( KVs, FKVs, drop_k(true) ).
+?- kv_compose_k_nth([no(1,2,3),no(4,2,6),no(7,8,9)], 2, KVs), kv_compose_k_frequency(KVs, FKVs, drop_k(true)).
 KVs = [2-no(1, 2, 3), 2-no(4, 2, 6), 8-no(7, 8, 9)],
-FKVs = [2-[no(1, 2, 3), no(4, 2, 6)], 1-no(7, 8, 9)].
+FKVs = [2-[no(1, 2, 3), no(4, 2, 6)], 1-[no(7, 8, 9)]].
 
 This predicate can be used to return the groupings only.
 ==
-?- 
-
-==
-
-If pack(pack_errors) is installed (the predicate just checks, it does not require the pack): 
-==
-?- kv_compose_k_frequency([1-a,1-x,2-b], FKVs, [keysort(maybe)]).
-ERROR: stoics_lib:kv_compose_k_frequency/3 @ option(keysort): Object of type: boolean expected, but found term: maybe.
-==
-
-If pack(pack_errors) is not installed: 
-==
-?- kv_compose_k_frequency([1-a,1-x,2-b], FKVs, [keysort(maybe)]).
-ERROR: Unhandled exception: Unknown message: type_error(for_option(keysort),should_be(boolean),found(maybe))
+?- kv_compose_k_frequency([1-y,2-b,1-x,1-a], FKVs, drop_f(true)).
+FKVs = [1-[y, x, a], 2-[b]].
+?- kv_compose_k_frequency([1-y,2-b,1-x,1-a], FKVs, [drop_f(true),keysort(false)]).
+FKVs = [1-[a, x, y], 2-[b]].
 ==
 
 The predicate can extract frequency and KVs on more general input structures by passing it options ki(Ki) and vi(Vi).
@@ -104,8 +96,19 @@ The predicate can extract frequency and KVs on more general input structures by 
 FKVs = [2-'1st'-[a, alpha], 2-'2nd'-[b, beta], 2-'3rd'-[c, gamma]].
 
 ?- a_bet(Abet), kv_compose_k_frequency(Abet,FKVs,[ki(3),vi(2)]).
-?- Abet = [en(1,a,xe),gr(1,alpha,xr),en(2,b,ye),gr(2,beta,yg),en(3,c,ze),gr(3,gamma,zg)], assert(a_bet(Abet)).
+FKVs = [3-en-[a, b, c], 3-gr-[alpha, beta, gamma]].
+==
 
+If pack(pack_errors) is installed, (the predicate just checks, it does not require the pack): 
+==
+?- kv_compose_k_frequency([1-a,1-x,2-b], FKVs, [keysort(maybe)]).
+ERROR: stoics_lib:kv_compose_k_frequency/3 @ option(keysort): Object of type: boolean expected, but found term: maybe.
+==
+
+If pack(pack_errors) is not installed: 
+==
+?- kv_compose_k_frequency([1-a,1-x,2-b], FKVs, [keysort(maybe)]).
+ERROR: Unhandled exception: Unknown message: type_error(for_option(keysort),should_be(boolean),found(maybe))
 ==
 
 @author nicos angelopoulos
@@ -148,8 +151,8 @@ kv_compose_k_frequency_sort(true, Ki, KVs, KVo) :-
           ;
           sort( Ki, @=<, KVs, KVo )
      ).
-kv_compose_k_frequency_sort(false, Ki, KVs, KVo) :-
-     sort( Ki, @=<, KVs, KVo ).
+kv_compose_k_frequency_sort(false, _Ki, KVs, KVo) :-
+     sort( KVs, KVo ).
 
 kv_compose_k_frequency_order( [], K, Fa, V, _Ki, _Vi, Dfkv, [FKV] ) :-
      reverse( V, ReV ),
