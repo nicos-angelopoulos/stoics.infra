dc7b5247a441a10f50da99b379307c38964dc69b
Nicos Angelopoulos <nicos@gmx.co.uk>
2026-03-07 22:27:58 +0000

    DOC:    delete prompt counts from copy-paste

--- stoics_lib/src/kv/kvs_k_update_v.pl
+++ stoics_lib/src/kv/kvs_k_update_v.pl
@@ -1,9 +1,9 @@
-/** kvs_k_update_v( +KVset, +K, +GoalNull, GoalUpd, -V, -NewV, -NVset ).
+/** kvs_k_update_v( +KVset, +K, +GoalNew, GoalUpd, -V, -NewV, -NVset ).
 
 Update or insert the V part of two arg sorted terms list KVset to NVset.
 
-GoalNull is called if K does not appear in KVset and GoalUpd when K does exist.
-First is via =|call(+GoalNull, +K, -NewV, -NewKV)|= and second via =|call( +GoalUpd, +K, +V, -NewV, -NewKV )|=.
+GoalNew is called if K does not appear in KVset and GoalUpd when K does exist.
+First is via =|call(+GoalNew, +K, -NewV, -NewKV)|= and second via =|call( +GoalUpd, +K, +V, -NewV, -NewKV )|=.
 In the former case, V remains unbound.
 
 ==  
@@ -31,11 +31,11 @@ V = 2,
 Nv = 3,
 Set = [a-1, b-3, c-2].
 
-105 ?- kvs_k_update_v( [a-1,b-2,c-2], d, v_val(0), v_plus(1), V, Nv, Set ).
+?- kvs_k_update_v( [a-1,b-2,c-2], d, v_val(0), v_plus(1), V, Nv, Set ).
 Nv = 0,
 Set = [a-1, b-2, c-2, d-0].
 
-103 ?- kvs_k_update_v( [a-1,b-2,c-2], 0, v_val(1), v_plus(1), V, Nv, Set ).
+?- kvs_k_update_v( [a-1,b-2,c-2], 0, v_val(1), v_plus(1), V, Nv, Set ).
 Nv = 1,
 Set = [0-1, a-1, b-2, c-2].
 ==
